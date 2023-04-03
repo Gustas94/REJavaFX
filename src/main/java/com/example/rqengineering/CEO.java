dffa6f622d5d446b549b1bdfd5671f87d655eab7
Gustas94 <gustas.ruzgys@gmail.com>
2023-04-03 23:37:41 +0200

    CEO working except prices of products.

--- src/main/java/com/example/rqengineering/CEO.java
+++ src/main/java/com/example/rqengineering/CEO.java
@@ -24,6 +24,38 @@ public class CEO implements Initializable {
     @FXML
     private TableColumn<InstallationTeam, JobClass> activeJobColumn;
 
+    @FXML
+    private TableView<JobClass> jobTable;
+    @FXML
+    private TableColumn<JobClass, Integer> jobIdColumn;
+    @FXML
+    private TableColumn<JobClass, String> nameColumn;
+    @FXML
+    private TableColumn<JobClass, String> isActiveColumn;
+    @FXML
+    private TableColumn<JobClass, String> descriptionColumn;
+
+    @FXML
+    private TableView<Customer> customerTable;
+    @FXML
+    private TableColumn<Customer, Integer> customerIdColumn;
+    @FXML
+    private TableColumn<Customer, String> firstNameColumn;
+    @FXML
+    private TableColumn<Customer, String> lastNameColumn;
+    @FXML
+    private TableColumn<Customer, String> emailColumn;
+    @FXML
+    private TableColumn<Customer, String> phoneNumberColumn;
+    @FXML
+    private TableColumn<Customer, String> addressColumn;
+    @FXML
+    private TableColumn<Customer, String> emergencyPhoneColumn;
+    @FXML
+    private TableColumn<Customer, String> customerActiveJobColumn;
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Set cell value factories
@@ -32,11 +64,36 @@ public class CEO implements Initializable {
         peopleColumn.setCellValueFactory(new PropertyValueFactory<>("amountOfPeople"));
         activeJobColumn.setCellValueFactory(new PropertyValueFactory<>("activeJob"));
 
+        jobIdColumn.setCellValueFactory(new PropertyValueFactory<>("jobId"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("jobName"));
+        isActiveColumn.setCellValueFactory(new PropertyValueFactory<>("active"));
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+        customerIdColumn.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        emergencyPhoneColumn.setCellValueFactory(new PropertyValueFactory<>("emergencyPhone"));
+        customerActiveJobColumn.setCellValueFactory(new PropertyValueFactory<>("activeJob"));
+
         loadInstallationTeams();
+        loadJobs();
+        loadCustomers();
     }
 
     private void loadInstallationTeams() {
         ObservableList<InstallationTeam> installationTeams = FXCollections.observableArrayList(Application.getTeams());
         installationTeamTable.setItems(installationTeams);
+    }
+
+    private void loadJobs() {
+        ObservableList<JobClass> jobList = FXCollections.observableArrayList(Application.getJobs());
+        jobTable.setItems(jobList);
+    }
+    private void loadCustomers() {
+        ObservableList<Customer> customers = FXCollections.observableArrayList(Application.getCustomers());
+        customerTable.setItems(customers);
     }
 }
